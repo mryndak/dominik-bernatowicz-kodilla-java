b0970b873b70c238a2d712edd8098bc9e5a14a1e
Bernatowicz <bernatowicz.dom@gmail.com>
2021-04-11 10:39:40 +0200

    test klasy wybierającej parzyste liczby z kolekcji

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,26 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OddNumbersExterminator {
+
+    List<Integer> number = new LinkedList<>();
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+
+        if (numbers.size() > 1) {
+            for (int i = 0; i < numbers.size(); i++) {
+
+                if ((numbers.get(i) % 2) != 0) {
+                    numbers.remove(i);
+                }
+            }
+            return numbers;
+        }
+        else {
+            System.out.println("Error: EMPTY LIST");
+            return numbers;
+        }
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,2 +1,51 @@
-package com.kodilla.testing.collection;public class CollectionTestSuite {
+package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Test List: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test List: end");
+    }
+
+    @DisplayName("Checking empty lists")
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+
+        OddNumbersExterminator oddNumExt = new OddNumbersExterminator();
+        List<Integer> number = new LinkedList<>();
+
+        System.out.println(oddNumExt.exterminate(number));
+    }
+
+    @DisplayName("Checking normal lists")
+
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+
+        OddNumbersExterminator oddNumExt = new OddNumbersExterminator();
+        List<Integer> number = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+//        number.add(1);
+//        number.add(2);
+//        number.add(3);
+//        number.add(4);
+//        number.add(5);
+
+        System.out.println(oddNumExt.exterminate(number));
+    }
 }
