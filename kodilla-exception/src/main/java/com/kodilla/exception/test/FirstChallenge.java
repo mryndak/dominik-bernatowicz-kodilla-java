c3aa33b832da30135cb805367f71f03d6b92b9f7
Bernatowicz <bernatowicz.dom@gmail.com>
2021-04-25 22:00:55 +0200

    Zadanie 8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = 0;
+        try {
+            result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e);
+        }
+        finally {
+            if (result == 0) {
+                System.out.println("You cannot divide by zero!");
+            }else {
+                System.out.println(result);
+            }
+        }
+    }
+}
