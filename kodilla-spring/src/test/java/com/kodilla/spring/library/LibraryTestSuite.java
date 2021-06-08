3610beff7977e3a53364f7adfc07ba56b5aa1d93
Bernatowicz <bernatowicz.dom@gmail.com>
2021-06-08 18:23:08 +0200

    14 dodatkowe

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -7,6 +7,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -35,5 +37,18 @@ class LibraryTestSuite {
 
         //Then
         //do nothing
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 }
